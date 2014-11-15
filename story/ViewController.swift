a14f653e1534ac86f3b737a1732daf48f84ef8e4
Osamu Nomura <hugo.strikes.back@gmail.com>
2014-11-15 12:39:48 +0900

    Initial Commit

---
+++ story/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  story
+//
+//  Created by 野村修 on 2014/11/15.
+//  Copyright (c) 2014年 ofellabuta. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ storyTests/storyTests.swift
@@ -0,0 +1,36 @@
+//
+//  storyTests.swift
+//  storyTests
+//
+//  Created by 野村修 on 2014/11/15.
+//  Copyright (c) 2014年 ofellabuta. All rights reserved.
+//
+
+import Cocoa
+import XCTest
+
+class storyTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
